913763a9e34decf3db6f1404b48d1de63d4d826f
Mohd Aman Naim Patel <ap5277478@gmail.com>
2025-02-05 20:43:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,15 @@ const __dirname = path.resolve();
 config(); // Load environment variables
 const app = express();
 
-// Passport configuration (ensure you've set up Passport strategies in './config/Passport.config.js')
-
-// Middleware
 app.use(cookieParser()); // To parse cookies
-app.use(express.json()); // To parse incoming JSON requests
 
-// Passport initialization
+app.use(express.json()); // To parse incoming JSON requests
 
 // Authentication Routes
 app.use("/auth", authRoutes); // Use auth routes from auth.routes.js
 
-// Apply the verifyUser middleware to the /url route at a global level
 app.use("/auth/url", urlRoutes); // Protect /url routes with verifyUser middleware
+
 app.use("/ping", (req, res) => {
   res.send("hello world");
 });
